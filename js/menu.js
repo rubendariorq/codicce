eba49f4abf1a469183771101f2adb876b20f5a0e
Ruben D Rodriguez Q <proyecto.rubendariorq@gmail.com>
2023-05-08 19:20:16 -0500

    Ajustes responsive y menu

--- js/menu.js
+++ js/menu.js
@@ -2,12 +2,19 @@
     const openButton = document.querySelector('.nav__menu');
     const menu = document.querySelector('.nav__link');
     const closeMenu = document.querySelector('.nav__close');
+    const itemsMenu = document.querySelectorAll('.nav__links');
 
-    openButton.addEventListener('click', ()=> {
+    openButton.addEventListener('click', () => {
         menu.classList.add('nav__link--show');
     })
 
-    closeMenu.addEventListener('click', ()=> {
+    closeMenu.addEventListener('click', () => {
         menu.classList.remove('nav__link--show');
     })
+
+    for (let i = 0; i < itemsMenu.length; i++) {
+        itemsMenu[i].addEventListener("click", () => {
+            menu.classList.remove('nav__link--show')
+        });
+    }
 })();
